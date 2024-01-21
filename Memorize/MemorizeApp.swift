06bc07490c2033524f91099424e9b62d56710405
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2024-01-21 16:03:47 +0300

    memorize start

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bakhtiyorjon Mirzajonov on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
